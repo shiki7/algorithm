973a1917060bd88573d11d47c85c17d9f5db882d
shiki7 <bitbite8@gmail.com>
2019-10-03 09:12:34 +0900

    Create trivial_division.py

---
+++ prime_factorization/trivial_division.py
@@ -0,0 +1,16 @@
+# 素因数分解（試し割り法）
+def trivial_division(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
