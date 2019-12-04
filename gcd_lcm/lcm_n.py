329acfa7692dd7ada7948b3a52d5de173ba882e8
shiki7 <bitbite8@gmail.com>
2019-12-04 09:32:20 +0900

    Update lcm_n.py

--- gcd_lcm/lcm_n.py
+++ gcd_lcm/lcm_n.py
@@ -1,9 +1,15 @@
-import math
+# バージョンによってはmath.gcdが使えないので定義する
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    while a % b != 0:
+        a, b = b, a % b
+    return b
 
 def lcm(a):
     x = a[0]
     for i in range(1, len(a)):
-        x = x * a[i] // math.gcd(x, a[i])
+        x = x * a[i] // gcd(x, a[i])
     return x
 
 a = list(map(int, input().split()))
