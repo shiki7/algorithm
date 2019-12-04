1f2059b83b31ec14edc9ec7340ad6c66cec34fda
shiki7 <bitbite8@gmail.com>
2019-12-04 09:28:27 +0900

    Update lcm_n.py

--- gcd_lcm/lcm_n.py
+++ gcd_lcm/lcm_n.py
@@ -4,7 +4,7 @@ def lcm(a):
     x = a[0]
     for i in range(1, len(a)):
         x = x * a[i] // math.gcd(x, a[i])
-        return x
+    return x
 
 a = list(map(int, input().split()))
 print(lcm(a))
