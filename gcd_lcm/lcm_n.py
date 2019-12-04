8e4555c2d07a947d9c19cea05b4bfdd7a1e7f8e5
shiki7 <bitbite8@gmail.com>
2019-12-04 09:23:26 +0900

    Update lcm_n.py

--- gcd_lcm/lcm_n.py
+++ gcd_lcm/lcm_n.py
@@ -1,9 +1,9 @@
-import fractions
+import math
 
 def lcm(a):
     x = a[0]
     for i in range(1, len(a)):
-        x = x * a[i] // fractions.gcd(x, a[i])
+        x = x * a[i] // math.gcd(x, a[i])
         return x
 
 a = list(map(int, input().split()))
