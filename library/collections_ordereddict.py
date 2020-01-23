3036a51f59fb702c45ca4fb1a4f9f15ddbbaffb9
shiki7 <bitbite8@gmail.com>
2020-01-23 12:16:30 +0900

    Create collections_ordereddict.py

---
+++ library/collections_ordereddict.py
@@ -0,0 +1,11 @@
+# 順序付き辞書(挿入した順番を記憶）
+from collections import OrderedDict
+
+od = collections.OrderedDict()
+
+od['k1'] = 1
+od['k2'] = 2
+od['k3'] = 3
+
+print(od)
+# OrderedDict([('k1', 1), ('k2', 2), ('k3', 3)])
