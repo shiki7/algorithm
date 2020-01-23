b3830f33442beb602eb96ecaa7f0b3d21c870da2
shiki7 <bitbite8@gmail.com>
2020-01-24 07:47:09 +0900

    Create divisor.py

---
+++ divisor/divisor.py
@@ -0,0 +1,9 @@
+# 約数を求める
+def make_divisors(n):
+    divisors = []
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            divisors.append(i)
+            if i != n // i:
+                divisors.append(n//i)
+    return divisor
