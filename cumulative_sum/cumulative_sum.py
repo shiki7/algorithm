e5d3a2a0b2458069f22722c1f5715d3d0a9730c8
shiki7 <bitbite8@gmail.com>
2020-01-30 19:40:17 +0900

    Update cumulative_sum.py

--- cumulative_sum/cumulative_sum.py
+++ cumulative_sum/cumulative_sum.py
@@ -2,3 +2,7 @@
 input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 ans = [sum(input_list[:i + 1]) for i in range(len(input_list))]
 print(ans)
+
+# 上記の場合にTLEする場合は以下を使う
+import itertool
+ans = list(itertools.accumulate(input_list))
