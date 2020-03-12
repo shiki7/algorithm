f9ff4f6935aa95767a550a95d66c57a828662a57
shiki7 <bitbite8@gmail.com>
2020-03-12 17:59:02 +0900

    Update bitwise operation_ex.py

--- bitwise operation/bitwise operation_ex.py
+++ bitwise operation/bitwise operation_ex.py
@@ -10,3 +10,11 @@ for i in range(2 ** n):
         if ((i >> j) & 1):  # 順に右にシフトさせ最下位bitのチェックを行う
             bag.append(item[j][0])  # フラグが立っていたら bag に果物を詰める
     print(bag)
+
+    
+
+# itertoolsを使えば以下のようにも書ける
+# 3桁の[0,1]の組み合わせ
+import itertools
+for i in itertools.product([0, 1], repeat=3):
+    print(i)
